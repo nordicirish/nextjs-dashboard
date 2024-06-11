3778465e30e965087a8bdb854d8aaf287601a3ab
Roger Graham <rogergraham147@gmail.com>
2024-06-11 13:46:27 +0300

    Import { sql } from '@vercel/postgres' and add sql statement to insert form data

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,6 @@
 'use server';
 import { z } from 'zod';
+import { sql } from '@vercel/postgres';
  
 const FormSchema = z.object({
   id: z.string(),
@@ -18,6 +19,14 @@ export async function createInvoice(formData: FormData) {
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+//   store monetary values in cents in the database to eliminate JavaScript floating-point errors and ensure greater accuracy
+    const amountInCents = amount * 100;
+    // create a new date with the format "YYYY-MM-DD" for the invoice's creation date
+    const date = new Date().toISOString().split('T')[0];
+    await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
 }
 
   
