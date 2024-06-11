4e815aa7c01da00bd79e60e16dfb39a91a2f44fe
Roger Graham <rogergraham147@gmail.com>
2024-06-11 13:48:09 +0300

    Import { revalidatePath } from 'next/cache'; and call it to clear the invoices route cache

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,7 @@
 'use server';
 import { z } from 'zod';
 import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
  
 const FormSchema = z.object({
   id: z.string(),
@@ -27,6 +28,7 @@ export async function createInvoice(formData: FormData) {
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+  revalidatePath('/dashboard/invoices');
 }
 
   
