3ed13acb46f508bcb5adab24ae53e5ff610978e7
Roger Graham <rogergraham147@gmail.com>
2024-06-11 16:07:01 +0300

    Change deleteInvoice() add try catch to handle errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,17 +2,17 @@
 import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
-import { redirect } from 'next/navigation'
- 
+import { redirect } from 'next/navigation';
+
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string(),
-//   change amount from a string to a number while also validating its type.
+  //   change amount from a string to a number while also validating its type.
   amount: z.coerce.number(),
   status: z.enum(['pending', 'paid']),
   date: z.string(),
 });
-// omit id and date fields 
+// omit id and date fields
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 // Use Zod to update the expected types
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
@@ -23,19 +23,19 @@ export async function createInvoice(formData: FormData) {
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
-//   store monetary values in cents in the database to eliminate JavaScript floating-point errors and ensure greater accuracy
-    const amountInCents = amount * 100;
-    // create a new date with the format "YYYY-MM-DD" for the invoice's creation date
-    const date = new Date().toISOString().split('T')[0];
-    try {
+  //   store monetary values in cents in the database to eliminate JavaScript floating-point errors and ensure greater accuracy
+  const amountInCents = amount * 100;
+  // create a new date with the format "YYYY-MM-DD" for the invoice's creation date
+  const date = new Date().toISOString().split('T')[0];
+  try {
     // insert the new invoice into the database
     await sql`
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
   } catch (error) {
-    return
-    message: 'Database Error: Failed to create invoice.'
+    return;
+    message: 'Database Error: Failed to create invoice.';
   }
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
@@ -47,28 +47,30 @@ export async function updateInvoice(id: string, formData: FormData) {
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
- 
+
   const amountInCents = amount * 100;
- try {
-  await sql`
+  try {
+    await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
- } catch (error) {
-  return
-  message: 'Database Error: Failed to update invoice.'
- }
+  } catch (error) {
+    return;
+    message: 'Database Error: Failed to update invoice.';
+  }
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
 // action is being called in the /dashboard/invoices path, so don't need to call redirect. Calling revalidatePath will trigger a new server request and re-render the table
 export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
-  revalidatePath('/dashboard/invoices');
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    //revalidate path inside the try block as no redirect is needed if successful
+    revalidatePath('/dashboard/invoices');
+  } catch (error) {
+    return;
+    message: 'Database Error: Failed to delete invoice.';
+  }
 }
-
-
-
-  
