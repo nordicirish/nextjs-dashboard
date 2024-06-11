30d4e3233882ac0ad303e3d4d5a5fcdc62564b8c
Roger Graham <rogergraham147@gmail.com>
2024-06-11 13:40:20 +0300

    I mport Zod and define a schema that matches the the form object.
    This schema will validate the formData before saving it to a database.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,11 +1,23 @@
 'use server';
+import { z } from 'zod';
+ 
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+//   change amount from a string to a number while also validating its type.
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+// omit id and date fields 
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
-  const rawFormData = {
+  const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
-  };
-  // Test it out:
-  console.log(rawFormData);
+  });
 }
+
+  
