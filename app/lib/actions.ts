84cf2d35bec8417ae975ae6a89e5d0c4cae4a0c4
Roger Graham <rogergraham147@gmail.com>
2024-06-11 13:53:02 +0300

    import { redirect } from 'next/navigation' and call it to redirect the user back to the invoices main page after revalidatePath has fetched fresh data

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,6 +2,7 @@
 import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
  
 const FormSchema = z.object({
   id: z.string(),
@@ -29,6 +30,7 @@ export async function createInvoice(formData: FormData) {
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
   revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
 
   
