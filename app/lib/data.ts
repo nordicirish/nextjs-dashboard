23103b49b56f9fd623a996f324ff2958a48cc17b
Roger Graham <rogergraham147@gmail.com>
2024-06-20 14:29:34 +0300

    Add fetchCustomersPages() ITEMS_PER_PAGE value is utilised to calculate and return total customer record pages

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -173,7 +173,21 @@ export async function fetchFilteredInvoices(
   }
 }
 
-
+export async function fetchCustomersPages(query: string) {
+  try {
+    const count = await sql`SELECT COUNT(*)
+    FROM customers
+    WHERE
+      customers.name ILIKE ${`%${query}%`} OR
+      customers.email ILIKE ${`%${query}%`}
+    `;
+    const totalPages = Math.ceil(Number(count.rows[0].count) / ITEMS_PER_PAGE);
+    return totalPages;
+  } catch (error) {
+    console.error('Database Error:', error);
+    throw new Error('Failed to fetch total number of customers.');
+  }
+}
 
 export async function fetchInvoicesPages(query: string) {
   try {
