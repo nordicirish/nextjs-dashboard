91b8aed1bd820f6f20041f56518ded9b45f40784
Roger Graham <rogergraham147@gmail.com>
2024-06-27 19:25:53 +0300

    Update nextConfig to allow Image component to display vercel blob files

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'kv5f0metqv93o2md.public.blob.vercel-storage.com',
+        port: '',
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig;
