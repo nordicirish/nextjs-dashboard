2883eb8dfc2602f521a4fbcdd37485fd913cfbd9
Roger Graham <rogergraham147@gmail.com>
2024-07-01 15:46:39 +0300

    Remove unused fetchCustomers import

--- app/dashboard/customers/create/page.tsx
+++ app/dashboard/customers/create/page.tsx
@@ -1,14 +1,11 @@
 import Form from '@/app/ui/customers/create-form';
 import Breadcrumbs from '@/app/ui/customers/breadcrumbs';
-import { fetchCustomers } from '@/app/lib/data';
 import { Metadata } from 'next';
 export const metadata: Metadata = {
   title: 'Dashboard Customers Create',
 };
 
 export default async function Page() {
-  const customers = await fetchCustomers();
-
   return (
     <main>
       <Breadcrumbs
