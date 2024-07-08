2954ffe8a7af8acd246b872ae8a7a7f64b35e2b8
Roger Graham <rogergraham147@gmail.com>
2024-07-08 22:40:40 +0300

    Add DeleteCustomer button component

--- app/ui/customers/buttons.tsx
+++ app/ui/customers/buttons.tsx
@@ -1,6 +1,6 @@
 import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-// import { deleteInvoice } from '@/app/lib/actions';
+ import { deleteCustomer } from '@/app/lib/actions';
 
 export function CreateCustomer() {
   return (
@@ -25,14 +25,15 @@ export function UpdateCustomer({ id }: { id: string }) {
   );
 }
 
-// export function DeleteCustomer({ id }: { id: string }) {
-//   const deleteCustomerWithId = deleteCustomer.bind(null, id);
-//   return (
-//     <form action={deleteCustomerWithId}>
-//       <button className="rounded-md border p-2 hover:bg-gray-100">
-//         <span className="sr-only">Delete</span>
-//         <TrashIcon className="w-5" />
-//       </button>
-//     </form>
-//   );
-// }
+ export function DeleteCustomer({ id }: { id: string }) {
+  const deleteCustomerWithId = deleteCustomer.bind(null, id);
+   return (
+
+     <form action={deleteCustomerWithId}>
+     <button className="rounded-md border p-2 hover:bg-gray-100">
+        <span className="sr-only">Delete</span>
+         <TrashIcon className="w-5" />
+      </button>
+     </form>
+   );
+}
