7b4926ca57af10fbe4a406fb888570a64d14f0d0
Roger Graham <rogergraham147@gmail.com>
2024-07-18 15:26:26 +0300

    Add MesssageProvider to wrap child content and add MessageList Component to allow messages to be displayed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 import { Metadata } from 'next';
+import { MessageProvider } from '@/app/context/MessageContext';
+import MessageList from '@/app/components/MessageList';
 
 export const metadata: Metadata = {
   title: {
@@ -18,7 +20,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        <MessageProvider>
+          <MessageList />
+          {children}
+        </MessageProvider>
+      </body>
     </html>
   );
 }
