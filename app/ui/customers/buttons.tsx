f230e48e51fb47c746e068a112795f49ed2e88a1
Roger Graham <rogergraham147@gmail.com>
2024-07-18 16:36:09 +0300

    remove deleteCustomer(() as moved to delete-customer-button.tsx

--- app/ui/customers/buttons.tsx
+++ app/ui/customers/buttons.tsx
@@ -1,6 +1,5 @@
-import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
+import { PencilIcon, PlusIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
- import { deleteCustomer } from '@/app/lib/actions';
 
 export function CreateCustomer() {
   return (
@@ -23,17 +22,4 @@ export function UpdateCustomer({ id }: { id: string }) {
       <PencilIcon className="w-5" />
     </Link>
   );
-}
-
- export function DeleteCustomer({ id }: { id: string }) {
-  const deleteCustomerWithId = deleteCustomer.bind(null, id);
-   return (
-
-     <form action={deleteCustomerWithId}>
-     <button className="rounded-md border p-2 hover:bg-gray-100">
-        <span className="sr-only">Delete</span>
-         <TrashIcon className="w-5" />
-      </button>
-     </form>
-   );
 }
